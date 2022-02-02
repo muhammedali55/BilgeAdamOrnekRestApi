742e1d03e05b0e3c25a20ec43152f9a460602f9a
MuhammetAli <muhammedali55@gmail.com>
2022-02-02 16:53:16 +0300

    Veritabanı Bağlandı

--- src/main/java/com/bilgeadam/demo/controller/PersonController.java
+++ src/main/java/com/bilgeadam/demo/controller/PersonController.java
@@ -6,13 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/person")
 public class PersonController {
-
 
     @Autowired
     PersonService personService;
@@ -24,5 +26,18 @@ public class PersonController {
         return ResponseEntity.ok("İlk Spring REST API si");
     }
 
+    // http:localhost:8080/person/save
+    @PostMapping("/save")
+    public ResponseEntity<Void> save(String name,String tckimlik){
+        personService.save(Person.builder()
+                        .tckimlik(tckimlik)
+                        .name(name)
+                .build());
+        return  ResponseEntity.ok().build();
+    }
 
+    @GetMapping("/findall")
+    public ResponseEntity<List<Person>> findAll(){
+        return ResponseEntity.ok(personService.findAll());
+    }
 }
